4fea698c52eeb4c8b907182c6910c3425f7aa11f
BrancaYHielo <isnardgestion@gmail.com>
2018-06-07 17:27:09 -0300

    Sistema de informacion

---
+++ sistema/src/ar/com/branca/sistema/pojos/Venta.java
@@ -0,0 +1,43 @@
+package ar.com.branca.sistema.pojos;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Martín
+ */
+public class Venta {
+    
+    private int idVenta;
+    private double montoVenta;
+    private Date fechaVenta;
+
+    public Venta(double montoVenta, Date fechaVenta) {
+        this.montoVenta = montoVenta;
+        this.fechaVenta = fechaVenta;
+    }
+
+    public Date getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(Date fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public double getMontoVenta() {
+        return montoVenta;
+    }
+
+    public void setMontoVenta(double montoVenta) {
+        this.montoVenta = montoVenta;
+    } 
+}
